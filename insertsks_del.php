044b178f16555a0811a4694716d35b626835b20b
= <=>
2025-03-13 10:01:05 +0100

    kolejne zmiany
    Co-authored-by: Adam9870 <Adam9870@users.noreply.github.com>

--- insertsks_del.php
+++ insertsks_del.php
@@ -12,6 +12,8 @@
 			position: fixed;
 			top:50%;
 			left: 50%;
+			background-color: red;
+			display: none;
 		}
 	</style>
 </head>
@@ -31,12 +33,41 @@
     Imię: <input type="text" name="imie"><br>
 	Nazwisko: <input type="text" name="nazwisko"><br>
 	Klasa: <input type="text" name="klasa"><br>
+	Rok urodzenia: <input type="text" name="rokurodzenia"><br>
+    Wzrost: <input type="text" name="wzrost"><br>
     <input type="submit" value="zapisz" name="submitDodaj">
 </form>
 
 <div id="divEdytuj">
-	<form></form>
+	<form action="insertsks_del.php" method="post" >
+		<h3>Edytuj zawodnika</h3>
+		<input type="number" name="idE" id='idE' hidden><br> <!-- Trzeba potem ukryc -->
+		Imię: <input type="text" name="imieE"><br>
+		Nazwisko: <input type="text" name="nazwiskoE"><br>
+		Klasa: <input type="text" name="klasaE"><br>
+		Rok urodzenia: <input type="text" name="rokurodzeniaE"><br> <!-- Added field -->
+        Wzrost: <input type="text" name="wzrostE"><br> <!-- Added field -->
+		<input type="submit" value="zapisz" name="submitEdytuj">
+
+
+	</form>
 </div>
+<!--  Ten skrypt trzeba gdzieś prZenieść -->
+<script>
+	const divEdytuj = document.getElementById('divEdytuj')
+
+	function edytuj(id, imie, nazwisko, klasa, rokurodzenia, wzrost) {
+    document.getElementById('idE').value = id;
+    document.getElementsByName('imieE')[0].value = imie;
+    document.getElementsByName('nazwiskoE')[0].value = nazwisko;
+    document.getElementsByName('klasaE')[0].value = klasa;
+    document.getElementsByName('rokurodzeniaE')[0].value = rokurodzenia; // Set the year of birth
+    document.getElementsByName('wzrostE')[0].value = wzrost; // Set the height
+    divEdytuj.style.display = 'block';
+}
+
+
+</script>
 
 
 <?php
@@ -56,9 +87,11 @@
 		$imie = $_POST['imie'];
 		$nazwisko = $_POST['nazwisko'];
 		$klasa = $_POST['klasa'];
+		$wzrost = $_POST['wzrost'];
+		$rokurodzenia = $_POST['rokurodzenia'];
 
-		$q_dodaj = "INSERT INTO zawodnicy (imie, nazwisko, klasa)
-			VALUES ('$imie', '$nazwisko', '$klasa')";
+		$q_dodaj = "INSERT INTO zawodnicy (imie, nazwisko, klasa, rokurodzenia, wzrost)
+			VALUES ('$imie', '$nazwisko', '$klasa', '$rokurodzenia','$wzrost')";
 		
 		if (mysqli_query($conn, $q_dodaj)) {
 			echo "<h3 class='komunikat'>Dodano rekord</h3>";
@@ -69,17 +102,28 @@
 
 
 
-	// if(isset($_POST['submitEdytuj'])){
+	if(isset($_POST['submitEdytuj'])){
 
 
-	// 	$q_edytuj = "UPDATE zawodnicy SET imie=$_POST['wpisane_imie'],nazwisko=$_POST['wpisane_nazwisko'],klasa=$_POST['wpisana_klasa'],rokurodzenia=$_POST['wpisany_rokurodzenia'],wzrost=$_POST['wpisany_wzrost'] WHERE id=$_POST['wpisane_id']";
-		
-	// 	if (mysqli_query($conn, $q_edytuj)) {
-	// 		echo "<h3 class='komunikat'>Zedytowano rekord</h3>";
-	// 	} else {
-	// 		echo "Error: " . $q_edytuj . "<br>" . mysqli_error($conn);
-	// 	}
-	// }
+	$q_edytuj = "UPDATE zawodnicy SET 
+    imie='{$_POST['imieE']}',
+    nazwisko='{$_POST['nazwiskoE']}',
+    klasa='{$_POST['klasaE']}',
+    rokurodzenia='{$_POST['rokurodzeniaE']}',
+    wzrost='{$_POST['wzrostE']}' 
+    WHERE id='{$_POST['idE']}'";
+
+		if (mysqli_query($conn, $q_edytuj)) {
+			echo "<h3 class='komunikat'>Zedytowano rekord</h3>";
+		} else {
+			echo "Error: " . $q_edytuj . "<br>" . mysqli_error($conn);
+		}
+	}
+
+
+	
+
+	
 
 	if(isset($_POST['submitUsun'])){
 		$id = $_POST['submitUsun'];
@@ -126,7 +170,7 @@ SELECT https://www.w3schools.com/php/php_mysql_select.asp
 	$result = mysqli_query($conn, $q_wypisywanie);
 
 	if (mysqli_num_rows($result) > 0) {
-
+ 
 		echo <<< END
 		<table>
 		<tr>
@@ -136,7 +180,7 @@ SELECT https://www.w3schools.com/php/php_mysql_select.asp
 			<th>Klasa</th>
 			<th>Data urodzenia</th>
 			<th>Wzrost</th>
-			<th colspan=2 width=150px>TEMPBUTTON</th>
+			<th colspan=2></th>
 		</tr>
 		END;
 		
@@ -153,10 +197,9 @@ SELECT https://www.w3schools.com/php/php_mysql_select.asp
 				<td>{$row['rokurodzenia']}</td>
 				<td>{$row['wzrost']}</td>
 				<td>
-					<button onclick='edytuj({$row['id']})' class='buttonEdytuj'>✏</button>
+					<button type='submit' value="zapisz" name='submitEdytuj' onclick='edytuj({$row['id']}, "{$row['imie']}", "{$row['nazwisko']}", "{$row['klasa']}", "{$row['rokurodzenia']}", "{$row['wzrost']}")' class='buttonEdytuj'>✏</button>
 				</td>
 				<td>
-
 					<form action="insertsks_del.php" method="post">
 						<button type='submit' value='{$row['id']}' class='buttonUsun' name='submitUsun'>🗑</button>
 					</form>
